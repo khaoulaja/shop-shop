e7e5fa6d8cb9b069304ba66903891442cee5febc
khaoula <khaoulajabour4@gmail.com>
2022-09-07 10:14:04 -0400

    Add CurrentCategory and categories reducers

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -2,7 +2,7 @@
 import{
     UPDATE_PRODUCTS,
     UPDATE_CATEGORIES,
-    UPDATE_CURRENT_CATEGORIE
+    UPDATE_CURRENT_CATEGORY
 } from '../utils/actions';
 
 import { reducer } from '../utils/reducers';
@@ -12,7 +12,7 @@ import { reducer } from '../utils/reducers';
 const initialState = {
     products: [],
     categories: [{name: 'Food'}], 
-    currentCategorie: '1',
+    currentCategory: '1',
 
 };
 
@@ -24,4 +24,27 @@ test('UPDATE_PRODUCTS', () => {
 
     expect(newState.products.length).toBe(2);
     expect(initialState.products.length).toBe(0);
+});
+
+test('UPDATE_CATEGORIES', ()=>{
+    let newState = reducer(initialState, {
+        type: UPDATE_CATEGORIES,
+        categories: [{},{}]
+    }
+    );
+
+    expect(newState.categories.length).toBe(2);
+    expect(initialState.categories.length).toBe(1);
+
+});
+test('UPDATE_CURRENT_CATEGORY', ()=>{
+    let newState = reducer(initialState, {
+        type: UPDATE_CURRENT_CATEGORY,
+        currentCategory: '2'
+    }
+    );
+
+    expect(newState.currentCategory).toBe('2');
+    expect(initialState.currentCategory).toBe('1');
+
 });
